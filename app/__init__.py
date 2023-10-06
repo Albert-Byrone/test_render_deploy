7b29cb1672598237edda1de792d9176e4bffb72b
Albert Byrone <albertbyrone1677@gmail.com>
2023-10-06 09:44:20 +0300

    test render

--- app/__init__.py
+++ app/__init__.py
@@ -3,20 +3,22 @@ import os
 from flask import Flask, jsonify, make_response
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
-from flask_sqlalchemy import SQLAlchemy
+# from flask_sqlalchemy import SQLAlchemy
+
+print(os.environ.get('DATABASE_URI'))
+
 
 
 def create_app():
   app = Flask(__name__)
-  db = SQLAlchemy(app)
-  db.init_app(app)
+  # db = SQLAlchemy(app)
   api = Api(app)
 
 
   app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
   app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
   app.json.compact = False
-
+  db.init_app(app)
   migrate = Migrate(app, db)
 
 
--- app/routes.py
+++ app/routes.py
@@ -14,3 +14,4 @@ class Birds(Resource):
         return make_response(jsonify(birds), 200)
 
 api.add_resource(Birds, '/birds')
+# api.add_resource(Birds, '/birds')

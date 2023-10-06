458c31e5ccf019d035b0e06d28362169566eee1d
Albert Byrone <albertbyrone1677@gmail.com>
2023-10-06 09:01:32 +0300

    test render

---
+++ app/routes.py
@@ -0,0 +1,15 @@
+import os
+
+from flask import Flask, jsonify, make_response
+from flask_migrate import Migrate
+from flask_restful import Api, Resource
+
+from models import db, Bird
+
+class Birds(Resource):
+
+    def get(self):
+        birds = [bird.to_dict() for bird in Bird.query.all()]
+        return make_response(jsonify(birds), 200)
+
+api.add_resource(Birds, '/birds')
